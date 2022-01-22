e21aa7853e0670bf93aec2bfcfe83a7d4a6399e6
mxmlnkn <mxmlnkn@github.de>
2022-01-22 10:11:57 +0100

    Add getMinPositive, getMax, reverseBits to common

--- indexed_bzip2/common.hpp
+++ indexed_bzip2/common.hpp
@@ -329,7 +329,7 @@ isLittleEndian()
 }
 
 
-inline uint64_t
+constexpr uint64_t
 byteSwap( uint64_t value )
 {
     value = ( ( value & uint64_t( 0x0000'0000'FFFF'FFFFULL ) ) << 32 ) |
@@ -341,7 +341,7 @@ byteSwap( uint64_t value )
     return value;
 }
 
-inline uint32_t
+constexpr uint32_t
 byteSwap( uint32_t value )
 {
     value = ( ( value & uint32_t( 0x0000'FFFFUL ) ) << 16 ) | ( ( value & uint32_t( 0xFFFF'0000UL ) ) >> 16 );
@@ -349,7 +349,7 @@ byteSwap( uint32_t value )
     return value;
 }
 
-inline uint16_t
+constexpr uint16_t
 byteSwap( uint16_t value )
 {
     value = ( ( value & uint16_t( 0x00FFU ) ) << 8  ) | ( ( value & uint16_t( 0xFF00U ) ) >> 8  );
@@ -522,3 +522,130 @@ private:
     const T* const m_data;
     const size_t   m_size;
 };
+
+
+template<typename T>
+static constexpr typename T::value_type
+getMinPositive( const T& container )
+{
+    if ( container.empty() ) {
+        throw std::invalid_argument( "Container must not be empty!" );
+    }
+
+    auto result = container.front();
+    for ( const auto value : container ) {
+        if ( value > 0 ) {
+            if ( result > 0 ) {
+                result = std::min( result, value );
+            } else {
+                result = value;
+            }
+        }
+    }
+    return result;
+}
+
+template<typename T>
+static constexpr typename T::value_type
+getMax( const T& container )
+{
+    const auto match = std::max_element( container.begin(), container.end() );
+    if ( match == container.end() ) {
+        throw std::invalid_argument( "Container must not be empty!" );
+    }
+    return *match;
+}
+
+
+constexpr uint8_t
+reverseBits( uint8_t data )
+{
+    /* Reverse bits using bit-parallelism in a recursive fashion, i.e., first swap every bit with its neighbor,
+     * then swap each half nibble with its neighbor, then each nibble. */
+    constexpr std::array<uint8_t, 3> masks = { 0b0101'0101U, 0b0011'0011U, 0b0000'1111U };
+    for ( uint8_t i = 0; i < masks.size(); ++i ) {
+        data = ( ( data &  masks[i] ) << ( 1U << i ) ) |
+               ( ( data & ~masks[i] ) >> ( 1U << i ) );
+    }
+    return data;
+}
+
+
+constexpr uint16_t
+reverseBits( uint16_t data )
+{
+    /* Reverse bits using bit-parallelism in a recursive fashion, i.e., first swap every bit with its neighbor,
+     * then swap each half nibble with its neighbor, then each nibble, then each byte. */
+    constexpr std::array<uint16_t, 4> masks = {
+        0b0101'0101'0101'0101U,
+        0b0011'0011'0011'0011U,
+        0b0000'1111'0000'1111U,
+        0b0000'0000'1111'1111U,
+    };
+    for ( uint8_t i = 0; i < masks.size(); ++i ) {
+        data = ( ( data &  masks[i] ) << ( 1U << i ) ) |
+               ( ( data & ~masks[i] ) >> ( 1U << i ) );
+    }
+    return data;
+}
+
+
+constexpr uint32_t
+reverseBits( uint32_t data )
+{
+    /* Reverse bits using bit-parallelism in a recursive fashion, i.e., first swap every bit with its neighbor,
+     * then swap each half nibble with its neighbor, then each nibble, then each byte, then each word. */
+    constexpr std::array<uint32_t, 5> masks = {
+        0b0101'0101'0101'0101'0101'0101'0101'0101U,
+        0b0011'0011'0011'0011'0011'0011'0011'0011U,
+        0b0000'1111'0000'1111'0000'1111'0000'1111U,
+        0b0000'0000'1111'1111'0000'0000'1111'1111U,
+        0b0000'0000'0000'0000'1111'1111'1111'1111U,
+    };
+    for ( uint8_t i = 0; i < masks.size(); ++i ) {
+        data = ( ( data &  masks[i] ) << ( 1U << i ) ) |
+               ( ( data & ~masks[i] ) >> ( 1U << i ) );
+    }
+    return data;
+}
+
+
+constexpr uint64_t
+reverseBits( uint64_t data )
+{
+    /* Reverse bits using bit-parallelism in a recursive fashion, i.e., first swap every bit with its neighbor,
+     * then swap each half nibble with its neighbor, then each nibble, then each byte, then each word, then each
+     * double word. */
+    constexpr std::array<uint64_t, 6> masks = {
+        0b0101'0101'0101'0101'0101'0101'0101'0101'0101'0101'0101'0101'0101'0101'0101'0101U,
+        0b0011'0011'0011'0011'0011'0011'0011'0011'0011'0011'0011'0011'0011'0011'0011'0011U,
+        0b0000'1111'0000'1111'0000'1111'0000'1111'0000'1111'0000'1111'0000'1111'0000'1111U,
+        0b0000'0000'1111'1111'0000'0000'1111'1111'0000'0000'1111'1111'0000'0000'1111'1111U,
+        0b0000'0000'0000'0000'1111'1111'1111'1111'0000'0000'0000'0000'1111'1111'1111'1111U,
+        0b0000'0000'0000'0000'0000'0000'0000'0000'1111'1111'1111'1111'1111'1111'1111'1111U,
+    };
+    /* Using godbolt, shows that both gcc 8.6 and clang will unroll this loop! Clang 13 seems to produce even shorter
+     * code by somehow skipping word and dword swaps, maybe implementing those via moves instead. Both, evaluate
+     * the 1U << i and ~masks[i] expressions at compile-time. Note that ~mask is -mask on two's-complement platforms! */
+    for ( uint8_t i = 0; i < masks.size(); ++i ) {
+        data = ( ( data &  masks[i] ) << ( 1U << i ) ) |
+               ( ( data & ~masks[i] ) >> ( 1U << i ) );
+    }
+    return data;
+}
+
+
+template<typename T>
+inline std::array<T, std::numeric_limits<T>::max()>
+createReversedBitsLUT()
+{
+    std::array<T, std::numeric_limits<T>::max()> result;
+    for ( uint16_t i = 0; i < result.size(); ++i ) {
+        result[i] = reverseBits( i );
+    }
+    return result;
+}
+
+
+alignas(8) static inline const std::array<uint16_t, std::numeric_limits<uint16_t>::max() >
+reversedBitsLUT16 = createReversedBitsLUT<uint16_t>();
