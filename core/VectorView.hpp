5521809cfe90b84dd98e12bfc4e533830dfe2144
mxmlnkn <mxmlnkn@github.de>
2022-02-09 00:24:12 +0100

    Add WeakVector and WeakArray

--- core/VectorView.hpp
+++ core/VectorView.hpp
@@ -4,6 +4,9 @@
 #include <stdexcept>
 
 
+/**
+ * Views are by their name read-only. This represents a read-only non-owned memory chunk.
+ */
 template<typename T>
 class VectorView
 {
@@ -85,6 +88,115 @@ private:
 };
 
 
+/**
+ * This is in basically a modifiable VectorView. The vector cannot be resized but
+ * it is a simple pointer and length tuple to a read-write memory chunk.
+ */
+template<typename T>
+class WeakVector
+{
+public:
+    using value_type = T;
+
+public:
+    WeakVector() = default;
+    WeakVector( const WeakVector& ) = default;
+    WeakVector( WeakVector&& ) = default;
+    WeakVector& operator=( const WeakVector& ) = default;
+    WeakVector& operator=( WeakVector&& ) = default;
+
+    WeakVector( const std::vector<T>& vector ) noexcept :
+        m_data( vector.data() ),
+        m_size( vector.size() )
+    {}
+
+    WeakVector( T*     data,
+                size_t size ) noexcept :
+        m_data( data ),
+        m_size( size )
+    {}
+
+    [[nodiscard]] T
+    front() const
+    {
+        return *m_data;
+    }
+
+    [[nodiscard]] const T*
+    begin() const noexcept
+    {
+        return m_data;
+    }
+
+    [[nodiscard]] T*
+    begin() noexcept
+    {
+        return m_data;
+    }
+
+    [[nodiscard]] const T*
+    end() const noexcept
+    {
+        return m_data + m_size;
+    }
+
+    [[nodiscard]] T*
+    end() noexcept
+    {
+        return m_data + m_size;
+    }
+
+    [[nodiscard]] T*
+    data() noexcept
+    {
+        return m_data;
+    }
+
+    [[nodiscard]] const T*
+    data() const noexcept
+    {
+        return m_data;
+    }
+
+    [[nodiscard]] size_t
+    size() const noexcept
+    {
+        return m_size;
+    }
+
+    [[nodiscard]] bool
+    empty() const noexcept
+    {
+        return m_size == 0;
+    }
+
+    [[nodiscard]] const T&
+    operator[]( size_t i ) const
+    {
+        return m_data[i];
+    }
+
+    [[nodiscard]] T&
+    operator[]( size_t i )
+    {
+        return m_data[i];
+    }
+
+    [[nodiscard]] T
+    at( size_t i ) const
+    {
+        if ( i >= m_size ) {
+            throw std::out_of_range( "VectorView index larger than size!" );
+        }
+        return m_data[i];
+    }
+
+private:
+    T* m_data{ nullptr };
+    size_t m_size{ 0 };
+};
+
+
 template<typename T,
          size_t   T_size>
 class ArrayView
@@ -93,6 +205,10 @@ public:
     using value_type = T;
 
 public:
+    ArrayView( const std::array<T, T_size>& array ) noexcept :
+        m_data( array.data() )
+    {}
+
     ArrayView( const T* data ) noexcept :
         m_data( data )
     {}
@@ -150,5 +266,86 @@ public:
 
 private:
     const T* const m_data;
+    constexpr static size_t m_size = T_size;
+};
+
+
+template<typename T,
+         size_t   T_size>
+class WeakArray
+{
+public:
+    using value_type = T;
+
+public:
+    WeakArray( T* data ) noexcept :
+        m_data( data )
+    {}
+
+    [[nodiscard]] T
+    front() const
+    {
+        return *m_data;
+    }
+
+    [[nodiscard]] const T*
+    begin() const noexcept
+    {
+        return m_data;
+    }
+
+    [[nodiscard]] const T*
+    end() const noexcept
+    {
+        return m_data + m_size;
+    }
+
+    [[nodiscard]] const T*
+    data() const noexcept
+    {
+        return m_data;
+    }
+
+    [[nodiscard]] T*
+    data() noexcept
+    {
+        return m_data;
+    }
+
+    [[nodiscard]] constexpr size_t
+    size() const noexcept
+    {
+        return m_size;
+    }
+
+    [[nodiscard]] bool
+    empty() const noexcept
+    {
+        return m_size == 0;
+    }
+
+    [[nodiscard]] const T&
+    operator[]( size_t i ) const
+    {
+        return m_data[i];
+    }
+
+    [[nodiscard]] T&
+    operator[]( size_t i )
+    {
+        return m_data[i];
+    }
+
+    [[nodiscard]] T
+    at( size_t i ) const
+    {
+        if ( i >= m_size ) {
+            throw std::out_of_range( "VectorView index larger than size!" );
+        }
+        return m_data[i];
+    }
+
+private:
+    T* const m_data;
     constexpr static size_t m_size = T_size;
 };
