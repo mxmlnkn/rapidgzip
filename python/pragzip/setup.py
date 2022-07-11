a53615a6de49e09dd011e0fe5cb9ea86cc181ed1
mxmlnkn <mxmlnkn@github.de>
2022-07-11 19:20:18 +0200

    [build] Add zlib source code to build statically especially on Windows

--- python/pragzip/setup.py
+++ python/pragzip/setup.py
@@ -19,11 +19,14 @@ except ImportError:
     cythonize = None
 
 
+zlib_sources = [ 'inflate.c', 'crc32.c', 'adler32.c', 'inftrees.c', 'inffast.c', 'zutil.c' ]
+zlib_sources = [ 'external/zlib/' + source for source in zlib_sources ]
+
 extensions = [
     Extension(
         name         = 'pragzip',
-        sources      = [ 'pragzip.pyx' ],
-        include_dirs = [ 'core', 'pragzip', 'pragzip/huffman' ],
+        sources      = [ 'pragzip.pyx' ] + zlib_sources,
+        include_dirs = [ 'core', 'pragzip', 'pragzip/huffman', 'external/zlib' ],
         language     = 'c++',
     ),
 ]
@@ -42,10 +45,40 @@ def supportsFlag(compiler, flag):
             return False
     return True
 
+def hasInclude(compiler, systemInclude):
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as file:
+        file.write(f'#include <{systemInclude}>\n' + 'int main() { return 0; }')
+        try:
+            compiler.compile([file.name])
+        except CompileError:
+            print(f"[Info] Check for {systemInclude} system header failed. Will try without out it. "
+                  "The above error can be ignored!")
+            return False
+    return True
+
 
 # https://github.com/cython/cython/blob/master/docs/src/tutorial/appendix.rst#python-38
 class Build(build_ext):
     def build_extensions(self):
+        # This is as hacky as it gets just in order to have different compile arguments for the zlib C-code as
+        # opposed to the C++ code but I don't see another way with this subpar "build system" if you can call
+        # it even that.
+        oldCompile = self.compiler.compile
+        def newCompile(sources, *args, **kwargs):
+            cSources = [source for source in sources if source.endswith('.c')]
+            cppSources = [source for source in sources if not source.endswith('.c')]
+
+            objects = oldCompile(cppSources, *args, **kwargs)
+            cppCompileArgs = ['-fconstexpr-ops-limit=99000100', '-fconstexpr-steps=99000100',
+                              '-std=c++17', '/std:c++17']
+            if 'extra_postargs' in kwargs:
+                kwargs['extra_postargs'] = [x for x in kwargs['extra_postargs'] if x not in cppCompileArgs]
+            cppObjects = oldCompile(cSources, *args, **kwargs)
+            objects.extend(cppObjects)
+            return objects
+
+        self.compiler.compile = newCompile
+
         for ext in self.extensions:
             ext.extra_compile_args = [ '-std=c++17', '-O3', '-DNDEBUG', '-DWITH_PYTHON_SUPPORT',
                                        '-D_LARGEFILE64_SOURCE=1' ]
@@ -75,6 +108,9 @@ class Build(build_ext):
                     ext.extra_compile_args += [ '-fconstexpr-ops-limit=99000100' ]
                 elif supportsFlag(self.compiler, '-fconstexpr-steps=99000100'):
                     ext.extra_compile_args += [ '-fconstexpr-steps=99000100' ]
+
+            if hasInclude(self.compiler, 'unistd.h'):
+                ext.extra_compile_args += ['-DZ_HAVE_UNISTD_H']
 
         super(Build, self).build_extensions()
 
