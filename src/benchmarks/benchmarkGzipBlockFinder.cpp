54a591728299da80d8cb7bb713b8f2850b501548
mxmlnkn <mxmlnkn@github.de>
2022-07-31 11:09:19 +0200

    [style] Fix clang-tidy misplaced widening cast warning

--- src/benchmarks/benchmarkGzipBlockFinder.cpp
+++ src/benchmarks/benchmarkGzipBlockFinder.cpp
@@ -711,7 +711,7 @@ findDeflateBlocksPragzipLUTTwoPass( BufferedFileReader::AlignedBuffer data )
     const auto checkOffset =
         [&block, &bitReader] ( const auto offset ) {
             try {
-                bitReader.seek( static_cast<long long int>( offset + 3 ) );
+                bitReader.seek( static_cast<long long int>( offset ) + 3 );
                 return block.readDynamicHuffmanCoding( bitReader ) == pragzip::Error::NONE;
             } catch ( const pragzip::BitReader::EndOfFileReached& ) {}
             return false;
