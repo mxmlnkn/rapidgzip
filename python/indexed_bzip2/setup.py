ca5f401cb0bb2b04cdeb16ba51908f80bad23ca7
mxmlnkn <mxmlnkn@github.de>
2022-11-10 23:35:04 +0100

    [style] Apply formatting to setup.py

--- python/indexed_bzip2/setup.py
+++ python/indexed_bzip2/setup.py
@@ -19,15 +19,17 @@ except ImportError:
 
 extensions = [
     Extension(
+        # fmt: off
         name         = 'indexed_bzip2',
-        sources      = [ 'indexed_bzip2.pyx' ],
-        include_dirs = [ '.', 'core', 'indexed_bzip2', 'external/cxxopts/include' ],
+        sources      = ['indexed_bzip2.pyx'],
+        include_dirs = ['.', 'core', 'indexed_bzip2', 'external/cxxopts/include'],
         language     = 'c++',
+        # fmt: on
     ),
 ]
 
 if cythonize:
-    extensions = cythonize( extensions, compiler_directives = { 'language_level' : '3' } )
+    extensions = cythonize(extensions, compiler_directives={'language_level': '3'})
 
 
 def supportsFlag(compiler, flag):
@@ -45,14 +47,14 @@ def supportsFlag(compiler, flag):
 class Build(build_ext):
     def build_extensions(self):
         for ext in self.extensions:
-            ext.extra_compile_args = [ '-std=c++17', '-O3', '-DNDEBUG', '-DWITH_PYTHON_SUPPORT' ]
+            ext.extra_compile_args = ['-std=c++17', '-O3', '-DNDEBUG', '-DWITH_PYTHON_SUPPORT']
 
             # https://github.com/cython/cython/issues/2670#issuecomment-432212671
             # https://github.com/cython/cython/issues/3405#issuecomment-596975159
             # https://bugs.python.org/issue35037
             # https://bugs.python.org/issue4709
-            if platform.system() == 'Windows' and platform.machine().endswith( '64' ):
-                ext.extra_compile_args += [ '-DMS_WIN64' ]
+            if platform.system() == 'Windows' and platform.machine().endswith('64'):
+                ext.extra_compile_args += ['-DMS_WIN64']
 
             if self.compiler.compiler_type == 'mingw32':
                 ext.extra_link_args = [
@@ -60,24 +62,31 @@ class Build(build_ext):
                     '-static-libstdc++',
                     '-Wl,-Bstatic,--whole-archive',
                     '-lwinpthread',
-                    '-Wl,--no-whole-archive'
+                    '-Wl,--no-whole-archive',
                 ]
 
             elif self.compiler.compiler_type == 'msvc':
-                ext.extra_compile_args = [ '/std:c++17', '/O2', '/DNDEBUG', '/DWITH_PYTHON_SUPPORT',
-                                           '/constexpr:steps99000100' ]
+                ext.extra_compile_args = [
+                    '/std:c++17',
+                    '/O2',
+                    '/DNDEBUG',
+                    '/DWITH_PYTHON_SUPPORT',
+                    '/constexpr:steps99000100',
+                ]
             else:
                 # The default limit is ~33 M (1<<25) and 99 M seem to be enough to compile currently on GCC 11.
                 if supportsFlag(self.compiler, '-fconstexpr-ops-limit=99000100'):
-                    ext.extra_compile_args += [ '-fconstexpr-ops-limit=99000100' ]
+                    ext.extra_compile_args += ['-fconstexpr-ops-limit=99000100']
                 elif supportsFlag(self.compiler, '-fconstexpr-steps=99000100'):
-                    ext.extra_compile_args += [ '-fconstexpr-steps=99000100' ]
+                    ext.extra_compile_args += ['-fconstexpr-steps=99000100']
 
         super(Build, self).build_extensions()
 
 
 setup(
-    py_modules  = [ 'indexed_bzip2' ],
+    # fmt: off
+    py_modules  = ['indexed_bzip2'],
     ext_modules = extensions,
-    cmdclass    = { 'build_ext': Build },
+    cmdclass    = {'build_ext': Build},
+    # fmt: on
 )
--- python/pragzip/setup.py
+++ python/pragzip/setup.py
@@ -17,20 +17,22 @@ except ImportError:
     cythonize = None
 
 
-zlib_sources = [ 'inflate.c', 'crc32.c', 'adler32.c', 'inftrees.c', 'inffast.c', 'zutil.c' ]
-zlib_sources = [ 'external/zlib/' + source for source in zlib_sources ]
+zlib_sources = ['inflate.c', 'crc32.c', 'adler32.c', 'inftrees.c', 'inffast.c', 'zutil.c']
+zlib_sources = ['external/zlib/' + source for source in zlib_sources]
 
 extensions = [
     Extension(
+        # fmt: off
         name         = 'pragzip',
-        sources      = [ 'pragzip.pyx' ] + zlib_sources,
-        include_dirs = [ '.', 'core', 'pragzip', 'pragzip/huffman', 'external/zlib', 'external/cxxopts/include' ],
+        sources      = ['pragzip.pyx'] + zlib_sources,
+        include_dirs = ['.', 'core', 'pragzip', 'pragzip/huffman', 'external/zlib', 'external/cxxopts/include'],
         language     = 'c++',
+        # fmt: on
     ),
 ]
 
 if cythonize:
-    extensions = cythonize( extensions, compiler_directives = { 'language_level' : '3' } )
+    extensions = cythonize(extensions, compiler_directives={'language_level': '3'})
 
 
 def supportsFlag(compiler, flag):
@@ -43,14 +45,17 @@ def supportsFlag(compiler, flag):
             return False
     return True
 
+
 def hasInclude(compiler, systemInclude):
     with tempfile.NamedTemporaryFile('w', suffix='.cpp') as file:
         file.write(f'#include <{systemInclude}>\n' + 'int main() { return 0; }')
         try:
             compiler.compile([file.name])
         except CompileError:
-            print(f"[Info] Check for {systemInclude} system header failed. Will try without out it. "
-                  "The above error can be ignored!")
+            print(
+                f"[Info] Check for {systemInclude} system header failed. Will try without out it. "
+                "The above error can be ignored!"
+            )
             return False
     return True
 
@@ -62,13 +67,18 @@ class Build(build_ext):
         # opposed to the C++ code but I don't see another way with this subpar "build system" if you can call
         # it even that.
         oldCompile = self.compiler.compile
+
         def newCompile(sources, *args, **kwargs):
             cSources = [source for source in sources if source.endswith('.c')]
             cppSources = [source for source in sources if not source.endswith('.c')]
 
             objects = oldCompile(cppSources, *args, **kwargs)
-            cppCompileArgs = ['-fconstexpr-ops-limit=99000100', '-fconstexpr-steps=99000100',
-                              '-std=c++17', '/std:c++17']
+            cppCompileArgs = [
+                '-fconstexpr-ops-limit=99000100',
+                '-fconstexpr-steps=99000100',
+                '-std=c++17',
+                '/std:c++17',
+            ]
             if 'extra_postargs' in kwargs:
                 kwargs['extra_postargs'] = [x for x in kwargs['extra_postargs'] if x not in cppCompileArgs]
             cppObjects = oldCompile(cSources, *args, **kwargs)
@@ -78,15 +88,20 @@ class Build(build_ext):
         self.compiler.compile = newCompile
 
         for ext in self.extensions:
-            ext.extra_compile_args = [ '-std=c++17', '-O3', '-DNDEBUG', '-DWITH_PYTHON_SUPPORT',
-                                       '-D_LARGEFILE64_SOURCE=1' ]
+            ext.extra_compile_args = [
+                '-std=c++17',
+                '-O3',
+                '-DNDEBUG',
+                '-DWITH_PYTHON_SUPPORT',
+                '-D_LARGEFILE64_SOURCE=1',
+            ]
 
             # https://github.com/cython/cython/issues/2670#issuecomment-432212671
             # https://github.com/cython/cython/issues/3405#issuecomment-596975159
             # https://bugs.python.org/issue35037
             # https://bugs.python.org/issue4709
-            if platform.system() == 'Windows' and platform.machine().endswith( '64' ):
-                ext.extra_compile_args += [ '-DMS_WIN64' ]
+            if platform.system() == 'Windows' and platform.machine().endswith('64'):
+                ext.extra_compile_args += ['-DMS_WIN64']
 
             if self.compiler.compiler_type == 'mingw32':
                 ext.extra_link_args = [
@@ -94,18 +109,23 @@ class Build(build_ext):
                     '-static-libstdc++',
                     '-Wl,-Bstatic,--whole-archive',
                     '-lwinpthread',
-                    '-Wl,--no-whole-archive'
+                    '-Wl,--no-whole-archive',
                 ]
 
             elif self.compiler.compiler_type == 'msvc':
-                ext.extra_compile_args = [ '/std:c++17', '/O2', '/DNDEBUG', '/DWITH_PYTHON_SUPPORT',
-                                           '/constexpr:steps99000100' ]
+                ext.extra_compile_args = [
+                    '/std:c++17',
+                    '/O2',
+                    '/DNDEBUG',
+                    '/DWITH_PYTHON_SUPPORT',
+                    '/constexpr:steps99000100',
+                ]
             else:
                 # The default limit is ~33 M (1<<25) and 99 M seem to be enough to compile currently on GCC 11.
                 if supportsFlag(self.compiler, '-fconstexpr-ops-limit=99000100'):
-                    ext.extra_compile_args += [ '-fconstexpr-ops-limit=99000100' ]
+                    ext.extra_compile_args += ['-fconstexpr-ops-limit=99000100']
                 elif supportsFlag(self.compiler, '-fconstexpr-steps=99000100'):
-                    ext.extra_compile_args += [ '-fconstexpr-steps=99000100' ]
+                    ext.extra_compile_args += ['-fconstexpr-steps=99000100']
 
             if hasInclude(self.compiler, 'unistd.h'):
                 ext.extra_compile_args += ['-DZ_HAVE_UNISTD_H']
@@ -114,6 +134,8 @@ class Build(build_ext):
 
 
 setup(
+    # fmt: off
     ext_modules = extensions,
-    cmdclass    = { 'build_ext': Build },
+    cmdclass    = {'build_ext': Build},
+    # fmt: on
 )
