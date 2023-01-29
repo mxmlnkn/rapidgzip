6edc3f1a021e30677930b2a3615c817f58dea2d2
mxmlnkn <mxmlnkn@github.de>
2023-01-29 15:37:49 +0100

    [refactor] Move code for copying non-compressed data into dedicated method

--- src/pragzip/deflate.hpp
+++ src/pragzip/deflate.hpp
@@ -545,6 +545,11 @@ private:
                   size_t     nMaxToDecode,
                   Window&    window );
 
+    template<typename Window>
+    [[nodiscard]] std::pair<size_t, Error>
+    readInternalUncompressed( BitReader& bitReader,
+                              Window&    window );
+
     template<typename Window,
              typename HuffmanCoding>
     [[nodiscard]] std::pair<size_t, Error>
@@ -1068,46 +1073,58 @@ Block<CALCULATE_CRC32, ENABLE_STATISTICS>::readInternal( BitReader& bitReader,
                                                          Window&    window )
 {
     if ( m_compressionType == CompressionType::UNCOMPRESSED ) {
-        /**
-         * Because the non-compressed deflate block size is 16-bit, the uncompressed data is limited to 65535 B!
-         * The buffer can hold MAX_WINDOW_SIZE 16-bit values (for markers) or twice the amount of decoded bytes.
-         * Therefore, this routine is safe to call in respect of "buffer overflows" before returning the view to
-         * the buffer.
-         * @note This does not take into account nMaxToDecode to avoid further state to keep track off.
-         * Timings for different buffer sizes in MB/s for 2GiB-random.gz:
-         * @verbatim
-         *    8 B : 398.55  411.779 409.841
-         *   16 B : 386.543 385.621 385.567
-         *   32 B : 412.783 407.354 402.352 402.129
-         *   64 B : 397.71  412.952 413.265 416.339
-         *  128 B : 379.629 380.691 387.439 377.22
-         *  256 B : 380.17  389.722 387.635 405.699
-         *  512 B : 382.466 379.642 390.317 381.801
-         * 1024 B : 384.92  386.544 381.748 388.71
-         * 2048 B : 378.362 394.002 391.357 389.728
-         * 4096 B : 380.87  379.09  386.711 395.955
-         * @endverbatim
-         */
-        uint32_t totalBytesRead{ 0 };
-        std::array<uint8_t, 64> buffer;
-        for ( ; totalBytesRead + buffer.size() <= m_uncompressedSize; totalBytesRead += buffer.size() ) {
-            const auto nBytesRead = bitReader.read( reinterpret_cast<char*>( buffer.data() ), buffer.size() );
-            for ( size_t i = 0; i < nBytesRead; ++i ) {
-                appendToWindow( window, buffer[i] );
-            }
-        }
-        for ( ; totalBytesRead < m_uncompressedSize; ++totalBytesRead ) {
-            appendToWindow( window, static_cast<uint8_t>( bitReader.read<BYTE_SIZE>() ) );
-        }
-        m_atEndOfBlock = true;
-        m_decodedBytes += m_uncompressedSize;
-        return { m_uncompressedSize, Error::NONE };
+        /* This does not take into account nMaxToDecode to avoid additional state to keep track off. */
+        return readInternalUncompressed( bitReader, window );
     }
 
     if ( m_compressionType == CompressionType::FIXED_HUFFMAN ) {
         return readInternalCompressed( bitReader, nMaxToDecode, window, m_fixedHC );
     }
     return readInternalCompressed( bitReader, nMaxToDecode, window, m_literalHC );
+}
+
+
+template<bool CALCULATE_CRC32,
+         bool ENABLE_STATISTICS>
+template<typename Window>
+std::pair<size_t, Error>
+Block<CALCULATE_CRC32, ENABLE_STATISTICS>::readInternalUncompressed( BitReader& bitReader,
+                                                                     Window&    window )
+{
+    /**
+     * Because the non-compressed deflate block size is 16-bit, the uncompressed data is limited to 65535 B!
+     * The buffer can hold MAX_WINDOW_SIZE 16-bit values (for markers) or twice the amount of decoded bytes.
+     * Therefore, this routine is safe to call in respect of "buffer overflows" before returning the view to
+     * the buffer.
+     *
+     * Timings for different buffer sizes in MB/s for 2GiB-random.gz:
+     * @verbatim
+     *    8 B : 398.55  411.779 409.841
+     *   16 B : 386.543 385.621 385.567
+     *   32 B : 412.783 407.354 402.352 402.129
+     *   64 B : 397.71  412.952 413.265 416.339
+     *  128 B : 379.629 380.691 387.439 377.22
+     *  256 B : 380.17  389.722 387.635 405.699
+     *  512 B : 382.466 379.642 390.317 381.801
+     * 1024 B : 384.92  386.544 381.748 388.71
+     * 2048 B : 378.362 394.002 391.357 389.728
+     * 4096 B : 380.87  379.09  386.711 395.955
+     * @endverbatim
+     */
+    uint32_t totalBytesRead{ 0 };
+    std::array<uint8_t, 64> buffer;
+    for ( ; totalBytesRead + buffer.size() <= m_uncompressedSize; totalBytesRead += buffer.size() ) {
+        const auto nBytesRead = bitReader.read( reinterpret_cast<char*>( buffer.data() ), buffer.size() );
+        for ( size_t i = 0; i < nBytesRead; ++i ) {
+            appendToWindow( window, buffer[i] );
+        }
+    }
+    for ( ; totalBytesRead < m_uncompressedSize; ++totalBytesRead ) {
+        appendToWindow( window, static_cast<uint8_t>( bitReader.read<BYTE_SIZE>() ) );
+    }
+    m_atEndOfBlock = true;
+    m_decodedBytes += m_uncompressedSize;
+    return { m_uncompressedSize, Error::NONE };
 }
 
 
