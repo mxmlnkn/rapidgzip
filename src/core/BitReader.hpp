mxmlnkn/rapidgzip

commit 496e2e719257bee6340fb7faea2dd886ce90905b

[feature] Factor out ensureSharedFileReader from BitReader to allow decompression from stdin
This enables:

   cat 4GiB-base64.gz | src/tools/pragzip -P 1 -d -o /dev/null

Before this commit, it resulted in:

   Caught exception:
   This class heavily relies on seeking and won't work with unseekable files!, typeid: St16invalid_argument

This still happens for the multi-threaded case like with -P 2.

It is not a bugfix because it did not work in 0.5.0, 0.4.0, 0.3.0,
0.2.0, 0.1.0.

Use [[deprecated]] to find calling locations:

 - 53 in total
 - 11 excluding src/tests and src/benchmarks

Those 11 calling locations:

    indexed_bzip2/BZ2Reader.hpp:41:9
    indexed_bzip2/ParallelBZ2Reader.hpp:50:9
    pragzip/blockfinder/PigzNaive.hpp:99:13
    pragzip/blockfinder/PigzParallel.hpp:179:92
    pragzip/blockfinder/PigzStringView.hpp:142:99
    pragzip/GzipAnalyzer.hpp:37:58
    pragzip/GzipBlockFinder.hpp:64:54
    pragzip/GzipReader.hpp:53:9
    pragzip/ParallelGzipReader.hpp:202:9
    tools/bitpatternstats.cpp:274:86
    tools/ibzip2.cpp:41:82

src/core/BitReader.hpp
@@ -21,8 +21,6 @@
 #include <BitManipulation.hpp>
 #include <common.hpp>
 #include <filereader/FileReader.hpp>
-#include <filereader/Standard.hpp>
-#include <filereader/Shared.hpp>
 
 
 /**
@@ -84,9 +82,7 @@ public:
 public:
     explicit
     BitReader( std::unique_ptr<FileReader> fileReader ) :
-        m_file( dynamic_cast<SharedFileReader*>( fileReader.get() ) == nullptr
-                ? std::unique_ptr<FileReader>( std::make_unique<SharedFileReader>( std::move( fileReader ) ) )
-                : std::move( fileReader ) )
+        m_file( std::move( fileReader ) )
     {}
 
     BitReader( BitReader&& other ) = default;
@@ -102,12 +98,6 @@ public:
             throw std::invalid_argument( "Copying BitReader to unseekable file not supported yet!" );
         }
         seek( other.tell() );
-    }
-
-    [[nodiscard]] std::unique_ptr<FileReader>
-    cloneSharedFileReader() const
-    {
-        return std::unique_ptr<FileReader>( m_file->clone() );
     }
 
     /* File Reader Interface Implementation */
@@ -467,14 +457,6 @@ public:
     bufferRefillCount() const
     {
         return m_bufferRefillCount;
-    }
-
-    void
-    setStatisticsEnabled( bool enabled )
-    {
-        if ( auto* const sharedFile = dynamic_cast<SharedFileReader*>( m_file.get() ); sharedFile != nullptr ) {
-            sharedFile->setStatisticsEnabled( enabled );
-        }
     }
 
 private:

src/indexed_bzip2/BZ2Reader.hpp
@@ -58,6 +58,15 @@ public:
     {}
 #endif
 
+    /* Forbid copying because it is hard to get right and there is not much use for it right now. */
+    BZ2Reader( const BZ2Reader& ) = delete;
+    BZ2Reader& operator=( const BZ2Reader& ) = delete;
+
+    /* Forbid moving because it is not used right now but it could probably be defaulted as long
+     * as BitReader has a working move constructor. */
+    BZ2Reader( BZ2Reader&& ) = delete;
+    BZ2Reader& operator=( BZ2Reader&& ) = delete;
+
     /* FileReader overrides */
 
     [[nodiscard]] FileReader*

src/core/filereader/Shared.hpp
@@ -332,3 +332,17 @@ private:
      */
     size_t m_currentPosition{ 0 };
 };
+
+
+[[nodiscard]] std::unique_ptr<SharedFileReader>
+ensureSharedFileReader( std::unique_ptr<FileReader>&& fileReader )
+{
+    if ( !fileReader ) {
+        throw std::invalid_argument( "File reader must not be null!" );
+    }
+
+    if ( dynamic_cast<SharedFileReader*>( fileReader.get() ) != nullptr ) {
+        return std::unique_ptr<SharedFileReader>( dynamic_cast<SharedFileReader*>( fileReader.release() ) );
+    }
+    return std::make_unique<SharedFileReader>( std::move( fileReader ) );
+}

src/indexed_bzip2/ParallelBZ2Reader.hpp
@@ -19,6 +19,7 @@
 #include <BlockMap.hpp>
 #include <common.hpp>
 #include <filereader/FileReader.hpp>
+#include <filereader/Shared.hpp>
 #include <ParallelBitStringFinder.hpp>
 
 #include "BZ2BlockFetcher.hpp"
@@ -27,6 +28,7 @@
 
 #ifdef WITH_PYTHON_SUPPORT
     #include <filereader/Python.hpp>
+    #include <filereader/Standard.hpp>
 #endif
 
 
@@ -47,14 +49,14 @@ public:
     explicit
     ParallelBZ2Reader( std::unique_ptr<FileReader> fileReader,
                        size_t                      parallelization = 0 ) :
-        m_bitReader( std::move( fileReader ) ),
+        m_sharedFileReader( ensureSharedFileReader( std::move( fileReader ) ) ),
         m_fetcherParallelization( parallelization == 0 ? availableCores() : parallelization ),
         m_startBlockFinder(
             [&] ()
             {
                 return std::make_shared<BlockFinder>(
                     std::make_unique<ParallelBitStringFinder<bzip2::MAGIC_BITS_SIZE> >(
-                        m_bitReader.cloneSharedFileReader(),
+                        std::unique_ptr<FileReader>( m_sharedFileReader->clone() ),
                         bzip2::MAGIC_BITS_BLOCK,
                         m_finderParallelization
                 ) );
@@ -485,7 +487,8 @@ private:
     }
 
 private:
-    BitReader m_bitReader;
+    const std::unique_ptr<SharedFileReader> m_sharedFileReader;
+    BitReader m_bitReader{ std::unique_ptr<FileReader>( m_sharedFileReader->clone() ) };
 
     size_t m_currentPosition = 0; /**< the current position as can only be modified with read or seek calls. */
     bool m_atEndOfFile = false;

src/pragzip/ParallelGzipReader.hpp
@@ -19,9 +19,11 @@
 #include <BlockMap.hpp>
 #include <common.hpp>
 #include <filereader/FileReader.hpp>
+#include <filereader/Shared.hpp>
 
 #ifdef WITH_PYTHON_SUPPORT
     #include <filereader/Python.hpp>
+    #include <filereader/Standard.hpp>
 #endif
 
 #include "crc32.hpp"
@@ -199,17 +201,17 @@ public:
     ParallelGzipReader( std::unique_ptr<FileReader> fileReader,
                         size_t                      parallelization = 0,
                         uint64_t                    chunkSizeInBytes = 4_Mi ) :
-        m_bitReader( std::move( fileReader ) ),
+        m_sharedFileReader( ensureSharedFileReader( std::move( fileReader ) ) ),
         m_fetcherParallelization( parallelization == 0 ? availableCores() : parallelization ),
         m_startBlockFinder(
             [this, chunkSizeInBytes] () {
                 return std::make_unique<BlockFinder>(
-                    m_bitReader.cloneSharedFileReader(),
+                    std::unique_ptr<FileReader>( m_sharedFileReader->clone() ),
                     /* spacing in bytes */ std::max( 8_Ki, chunkSizeInBytes ) );
             }
         )
     {
-        m_bitReader.setStatisticsEnabled( ENABLE_STATISTICS && SHOW_PROFILE );
+        m_sharedFileReader->setStatisticsEnabled( ENABLE_STATISTICS && SHOW_PROFILE );
         if ( !m_bitReader.seekable() ) {
             throw std::invalid_argument( "Parallel BZ2 Reader will not work on non-seekable input like stdin (yet)!" );
         }
@@ -815,7 +817,8 @@ private:
     }
 
 private:
-    BitReader m_bitReader;
+    const std::unique_ptr<SharedFileReader> m_sharedFileReader;
+    BitReader m_bitReader{ std::unique_ptr<FileReader>( m_sharedFileReader->clone() ) };
 
     size_t m_currentPosition = 0; /**< the current position as can only be modified with read or seek calls. */
     bool m_atEndOfFile = false;

src/tests/pragzip/testGzipChunkFetcher.cpp
@@ -49,7 +49,9 @@ testAutomaticMarkerResolution( const std::filesystem::path& filePath,
     std::cerr << "Test Automatic Marker Resolution with: " << filePath.filename()
               << " starting from block " << blockIndex << "\n";
 
-    pragzip::BitReader bitReader( std::make_unique<StandardFileReader>( filePath ) );
+    pragzip::BitReader bitReader(
+        std::make_unique<SharedFileReader>(
+            std::make_unique<StandardFileReader>( filePath ) ) );
     const auto blockOffset = getBlockOffset( filePath, blockIndex );
     try {
         std::atomic<bool> cancel{ false };

commit 79575f2dac9fd82aba4d194036c81b59158ee267

[fix] Protect BZ2Reader against external file position modifications
Bug got introduced with:

    496e2e71 2023-03-22 [feature] Factor out ensureSharedFileReader
    from BitReader to allow decompression from stdin

src/core/BitReader.hpp
@@ -83,7 +83,11 @@ public:
 public:
     explicit
     BitReader( UniqueFileReader fileReader ) :
-        m_file( std::move( fileReader ) )
+        /* The UniqueFileReader input argument sufficiently conveys and ensures that the file ownership is taken.
+         * But, because BitReader has a fileno getter returning the underlying fileno, it is possible that the
+         * file position is changed from the outside. To still keep correct behavior in that case, we have to
+         * to make it a SharedFileReader, which keeps track of the intended file position. */
+        m_file( ensureSharedFileReader( std::move( fileReader ) ) )
     {}
 
     BitReader( BitReader&& other ) = default;

src/core/ParallelBitStringFinder.hpp
@@ -19,7 +19,6 @@
 
 #include "AffinityHelpers.hpp"
 #include "BitStringFinder.hpp"
-#include "BitReader.hpp"
 #include "common.hpp"
 #include "ThreadPool.hpp"
 

src/indexed_bzip2/BZ2Reader.hpp
@@ -38,23 +38,29 @@ public:
 public:
     explicit
     BZ2Reader( UniqueFileReader fileReader ) :
-        m_bitReader( std::move( fileReader ) )
+        /* Without this, the test_joining_archive test inside ratarmountcore's test_factory.py fails.
+         * This is because tell() throws an exception because m_file.tell() is in an unexpected position.
+         * I'm still not sure why but maybe some external caller resets the file position to 0.
+         * As we are returning fileno to the underlying file and because the given PyObject input file object
+         * can have shared ownership, we need to ensure that this class works even when the file object is
+         * seeked from outside. */
+        m_bitReader( ensureSharedFileReader( std::move( fileReader ) ) )
     {}
 
 #ifdef WITH_PYTHON_SUPPORT
     explicit
     BZ2Reader( const std::string& filePath ) :
-        m_bitReader( std::make_unique<StandardFileReader>( filePath ) )
+        BZ2Reader( std::make_unique<StandardFileReader>( filePath ) )
     {}
 
     explicit
     BZ2Reader( int fileDescriptor ) :
-        m_bitReader( std::make_unique<StandardFileReader>( fileDescriptor ) )
+        BZ2Reader( std::make_unique<StandardFileReader>( fileDescriptor ) )
     {}
 
     explicit
     BZ2Reader( PyObject* pythonObject ) :
-        m_bitReader( std::make_unique<PythonFileReader>( pythonObject ) )
+        BZ2Reader( std::make_unique<PythonFileReader>( pythonObject ) )
     {}
 #endif
 