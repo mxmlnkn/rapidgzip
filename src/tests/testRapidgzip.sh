mxmlnkn/rapidgzip

commit 582d6f7d002f2f7970b7d83d5fc502131655875d

[fix] Make importing gztool index work with sequential read method to seekable archive file

src/core/filereader/SinglePass.hpp
@@ -229,6 +229,15 @@ public:
         if ( m_underlyingFileEOF ) {
             return m_numberOfBytesRead;
         }
+        if ( m_file ) {
+            const auto underlyingSize = m_file->size();
+            /* Unfortunately, StandardFileReader currently may simply return 0 instead of std::nullopt
+             * even for non-seekable stdin, therefore we also need to check for that! */
+            if ( underlyingSize && ( m_file->seekable() || ( *underlyingSize > 0 ) ) ) {
+                return *underlyingSize;
+            }
+            return m_fileno;
+        }
         return std::nullopt;
     }
 

commit ba82ab06cab6b29079108bb158277504f3171ac3

[fix] Reading from stdin was effectively broken
Introduced in 582d6f7d002f2f7970b7d83d5fc502131655875d because of dumb
copy-paste error.

src/core/filereader/SinglePass.hpp
@@ -236,7 +236,6 @@ public:
             if ( underlyingSize && ( m_file->seekable() || ( *underlyingSize > 0 ) ) ) {
                 return *underlyingSize;
             }
-            return m_fileno;
         }
         return std::nullopt;
     }

.github/workflows/test-cpp.yml
@@ -238,6 +238,8 @@ jobs:
         mkdir -p "build-sanitized-$sanitizer" && cd -- "$_"
         CXX=clang++ CC=clang cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-fsanitize=$sanitizer -DSHORT_TESTS" ..
         cmake --build . -- check
+        cmake --build .
+        bash ../src/tests/testRapidgzip.sh
 
   UBSan:
     name: UBSan

src/tests/testRapidgzip.sh
@@ -1,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Should be run from build folder
+
+
+RAPIDGZIP='src/tools/rapidgzip'
+export RAPIDGZIP
+
+function echoerr() { echo "$@" 1>&2; }
+
+returnError()
+{
+    local lineNumber message
+    if [ $# -eq 2 ]; then
+        lineNumber=:$1
+        message=$2
+    else
+        message=$*
+    fi
+
+    echoerr -e "\e[37m${FUNCNAME[1]}$lineNumber <- ${FUNCNAME[*]:2}\e[0m"
+    echoerr -e "\e[37m$message\e[0m"
+    echoerr -e '\e[31mTEST FAILED!\e[0m'
+
+    exit 1
+}
+
+
+# Tests with stdin are hard to do in Catch2, so do them here.
+[[ "$( cat ../src/tests/data/base64-256KiB.gz | $RAPIDGZIP --count )" -eq 262144 ]] ||
+    returnError "$LINENO" 'Stdin count test failed!'
+
+echo -e '\e[32mAll tests ran successfully.\e[0m'