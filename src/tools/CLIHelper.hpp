77a4ee84e95a099ab6d600e79cd404f745d98985
mxmlnkn <mxmlnkn@github.de>
2024-05-25 12:52:44 +0200

    [fix] Warn when specifying multiple output file paths

--- src/tools/CLIHelper.hpp
+++ src/tools/CLIHelper.hpp
@@ -9,6 +9,12 @@
 getFilePath( cxxopts::ParseResult const& parsedArgs,
              std::string          const& argument )
 {
+    if ( parsedArgs.count( argument ) > 1 ) {
+        if ( parsedArgs.count( "quiet" ) == 0 ) {
+            std::cerr << "[Warning] Multiple output files specified. Will only use the last one: "
+                      << parsedArgs[argument].as<std::string>() << "!\n";
+        }
+    }
     if ( parsedArgs.count( argument ) > 0 ) {
         auto path = parsedArgs[argument].as<std::string>();
         if ( path != "-" ) {
