4396eca76fb54b4dfbd2c2158d625f13df01b8b4
mxmlnkn <mxmlnkn@github.de>
2025-08-31 15:26:52 +0200

    [build] Move many things into submodule instead of duplicating it

--- results/benchmarks/rapidgzip-0.8.1-scaling-benchmarks-2023-08-06/createPlots.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd -- "$( dirname -- "$BASH_SOURCE[0]" )/plots"
-DISPLAY= python3 ../../../plotBenchmarks2023.py ../data/scaling-base64
-DISPLAY= python3 ../../../plotBenchmarks2023.py ../data/scaling-fastq
-DISPLAY= python3 ../../../plotBenchmarks2023.py ../data/scaling-silesia
--- results/benchmarks/rapidgzip-0.9.0-scaling-benchmarks-2023-08-30/createPlots.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p -- "$( dirname -- "$BASH_SOURCE[0]" )/plots" && cd -- "$_"
-for type in base64 fastq silesia; do (
-    mkdir -p -- "scaling-$type" &&
-    cd -- "$_" &&
-    DISPLAY= python3 ../../../../plotBenchmarks2023.py "../../data/scaling-$type"
-); done
